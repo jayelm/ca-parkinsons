2a7d82497121b5f2cac4894debe2d3b671152fdf
jmu303 <muj@bc.edu>
2015-06-26 17:19:48 +0200

    rerun biclustering plots

--- R/biclustering.R
+++ R/biclustering.R
@@ -10,13 +10,22 @@ source('./preprocessing.R')
 # Note - N can't be more than 16 for par, will need to fix that later
 # (multiple plots/pagination)
 N <- 16
+SAVE.PLOTS <- FALSE
 
 # BICLUSTERING ====
 # Bimax algorithm
 bcl <- biclust(as.matrix(raw.filtered), method=BCBimax(), minr=2, minc=2, number=N)
+if (SAVE.PLOTS) {
+  dev.copy(pdf, paste("../figures/biclust-", N, ".pdf", sep=""))
+  dev.off()
+}
 
 # BUBBLEPLOT (?) ====
 bubbleplot(raw.filtered, bcl)
+if (SAVE.PLOTS) {
+  dev.copy(pdf, paste("../figures/biclust-bubbleplot-", N, ".pdf", sep=""))
+  dev.off()
+}
 
 # CLUSTER HEATMAPS ====
 
@@ -25,3 +34,7 @@ par(mfrow = n2mfrow(N))
 for (i in 1:N) {
   drawHeatmap(raw.filtered, bicResult=bcl, number=i)
 }
+# if (SAVE.PLOTS) {
+#   dev.copy(pdf, paste("../figures/biclust-heatmap-", N, ".pdf", sep=""))
+#   dev.off()
+# }
