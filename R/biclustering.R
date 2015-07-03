26c6cd3b94bd527d41872dcaa2c44a85118d90fd
jmu303 <muj@bc.edu>
2015-07-03 16:50:08 +0200

    Try some different biclustering algorithms
    Nothing too revolutionary. I need to structure my search such that I'm
    looking for the right data.

--- R/biclustering.R
+++ R/biclustering.R
@@ -2,19 +2,29 @@
 # http://cran.r-project.org/web/packages/biclust/biclust.pdf
 library(biclust)  # Quite a recent package (May 12, 2015)
 
-# LOAD DATA ====
+# LOAD DATA, REMOVE PDONSET ====
 source('./preprocessing.R')
+raw.nopdonset <- raw.filtered
+raw.nopdonset.unscaled <- raw.filtered.unscaled
+raw.nopdonset$pdonset <- NULL
+raw.nopdonset.unscaled$pdonset <- NULL
 
 # CONSTANTS ====
 # Number of biclusters
 # Note - N can't be more than 16 for par, will need to fix that later
 # (multiple plots/pagination)
-N <- 16
+N <- 4
 SAVE.PLOTS <- FALSE
 
 # BICLUSTERING ====
 # Bimax algorithm
-bcl <- biclust(as.matrix(raw.filtered), method=BCBimax(), minr=2, minc=2, number=N)
+bcl <- biclust(as.matrix(raw.nopdonset), method=BCPlaid(), iter.startup = 50, iter.layer = 50)
+bcls <- bicluster(raw.nopdonset.unscaled, bcl)
+
+# Transform pdonset to number of years w/ parkinsons?
+# Then we want to look at some kind of comparison between NONMOTOR...
+# MOTOR...
+# and TIME to identify different TRAJECTORIES across time
 
 # BUBBLEPLOT (?) ====
 bubbleplot(raw.filtered, bcl)
