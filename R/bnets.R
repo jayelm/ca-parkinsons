756c803a5d75975bb381bf39ee378161837b5af4
jmu303 <muj@bc.edu>
2015-07-22 15:49:54 +0200

    Add Bayes nets for nms-dominated cluster 1

--- R/bnets.R
+++ R/bnets.R
@@ -2,6 +2,9 @@
 library(bnlearn)
 library(gRain) # Note - depends on biocLite graph and RBGL packages
 
+# Constants ====
+SAVE.BNETS <- F
+
 # Preprocessing ====
 source('./preprocessing.R')
 # Need stuff from kmeans dtree - so run that
@@ -22,9 +25,34 @@ plot(hc(clus4.df))
 mb(hc(clus4.df), 'cluster')
 
 # Learning only in c3 ====
+# These vars are set in exploration.R
 # Standardize to force gaussian vars
-c3.woclus <- c3[, -1]
-c3.std <- as.data.frame(scale(c3.woclus))
-c3.hc <- hc(c3.std)
-plot(c3.hc)
-mb(c3.hc, "cisitot")
+c1.woclus <- c1
+c1.woclus$class <- NULL
+c1.std <- as.data.frame(scale(c1.woclus))
+# Hill climbing ====
+c1.hc <- bnlearn::hc(c1.std)
+plot(c1.hc)
+title('Hill-Climbing Algorithm on Cluster 1')
+if (SAVE.BNETS) {
+  dev.copy(pdf, '../figures/clus1-bnet-hc.pdf')
+  dev.off()
+}
+# Grow shrink ====
+c1.gs <- bnlearn::gs(c1.std)
+plot(c1.gs)
+title('Grow-Shrink Algorithm on Cluster 1')
+if (SAVE.BNETS) {
+  dev.copy(pdf, '../figures/clus1-bnet-gs.pdf')
+  dev.off()
+}
+# Max-min hill-climbing ====
+c1.gs <- bnlearn::mmhc(c1.std)
+plot(c1.mmhc)
+title('Min-Max Hill Climbing Algorithm on Cluster 1')
+if (SAVE.BNETS) {
+  dev.copy(pdf, '../figures/clus1-bnet-mmhc.pdf')
+  dev.off()
+}
+
+mb(c1.hc, "cisitot")
