a90d2411c9de5db3b696e9905d5baa376b597b25
Jesse Mu <muj@bc.edu>
2016-02-20 18:11:23 -0500

    Improve longtiduinal graphs; position_dodge

--- R/longitudinal.R
+++ R/longitudinal.R
@@ -4,6 +4,9 @@
 library(ggplot2)
 library(gridExtra)
 library(grid)
+library(gtable)
+library(reshape2)
+library(plyr)
 
 library(ggthemes)
 source('./pubtheme.R')
@@ -21,15 +24,10 @@ qplot(raw.omitted[trainset.labeled$cluster == 3, ]$durat_pd)
 qplot(raw.omitted[trainset.labeled$cluster == 4, ]$durat_pd)
 
 # Bind durat_pd to 30 nms, CLUSTER PARAM===
-# MAKE SURE clus4.wide is correct!!
-# I don't think nms_d and nms1 are aligned right now!!!
-# TODO: Figure that out!!
-clus4.wide$cluster <- clusters.raw[]
-everything.wide <- clus4.wide
-everything.wide$durat_pd <- raw.omitted$durat_pd
+everything.wide <- raw.omitted
+everything.wide$cluster <- trainset.labeled$cluster
 nms.d <- c("nms_d1", "nms_d2", "nms_d3", "nms_d4", "nms_d5", "nms_d6",
            "nms_d7", "nms_d8", "nms_d9")
-everything.wide[, nms.d] <- raw.omitted[, nms.d]
 
 # Change this if you want to examine a different cluster!
 # use NULL if everything, and comment out subsetting
@@ -91,7 +89,7 @@ for (col in colnames(everything.wide)) {
   }
 }
 
-binned <- binned[!(binned$counts_k1 < 5 | binned$counts_k2 < 5 | binned$counts_k3 < 5 | binned$counts_k4 < 5), ]
+binned <- binned[!(binned$counts_k1 < 4 | binned$counts_k2 < 4 | binned$counts_k3 < 4 | binned$counts_k4 < 4), ]
 
 # Plot MULTI obs: setup ====
 
@@ -101,50 +99,82 @@ gg_color_hue <- function(n) {
   hcl(h=hues, l=65, c=100)[1:n]
 }
 
-library(reshape2)
-# Melt to long format for stacked bar chart
+# Melt to long format for multi.nms stacked bar chart ====
 binned.copy <- binned
 binned.copy$names <- rownames(binned.copy)
 binned.wide.pbar <- melt(binned.copy[, c("counts_k1", "counts_k2", "counts_k3", "counts_k4", "names")], id.var="names")
+# Rename factor, subtype
+levels(binned.wide.pbar$variable) <- c('1', '2', '3', '4')
+binned.wide.pbar <- rename(binned.wide.pbar, c('variable'='Subtype', 'value'='Counts'))
 rm(binned.copy)
 binned.wide.pbar$names <- factor(binned.wide.pbar$names, levels=rownames(binned))
-p.bar <- ggplot(binned.wide.pbar, aes(x = factor(names), y = value, fill = variable)) + 
+p.bar <- ggplot(binned.wide.pbar, aes(x = factor(names), y = Counts, fill = Subtype)) + 
   geom_bar(stat = "identity") +
   labs(x = "PD Duration") +
-  guides(fill = FALSE) +  # Guide already present in plot.nms
+  # guides(fill = FALSE) +  # Guide already present in plot.nms
   theme_pub() +
   theme(plot.margin = unit(c(0.5,1,1,1), "cm"))
 
-plot.nms <- function(nms_str, save=FALSE, means=FALSE) {
-  p <- ggplot(binned, aes(x = factor(1:nrow(binned))))
-  subclus.means <- c()
-  for (k in 1:4) {
-    k.factor <- as.factor(k)
-    aes_nms <- aes_string(x = "factor(1:nrow(binned))", y=paste(nms_str, "_k", k, sep=""), colour=k.factor)
-    aes_smooth <- aes_string(x = "as.numeric(factor(1:nrow(binned)))", y=paste(nms_str, "_k", k, sep=""), colour=k.factor)
-    aes_sd <- aes_string(
-      ymin=paste(paste(nms_str, "_k", k, sep=""), "-", nms_str, "_sd_k", k, sep=""),
-      ymax=paste(paste(nms_str, "_k", k, sep=""), "+", nms_str, "_sd_k", k, sep=""),
-      colour=k.factor
-    )
-    subclus.means <- c(subclus.means, mean(everything.wide[everything.wide$cluster == k, nms_str]))
-    p <- p +
-      geom_errorbar(aes_sd, width=0.25) +
-      geom_point(aes_nms) +
-      geom_smooth(aes_smooth, method="loess", se=FALSE)
-  }
-  # Subclus means
+# Melt to long format for multi.nms ====
+binned.copy <- binned
+binned.copy$names <- rownames(binned.copy)
+# Get variables we want
+measures <- names(binned.copy)[grep('.*sd.*|(names)', names(binned.copy), invert=T)]
+binned.wide.multi <- melt(
+  binned.copy,
+  id.var='names',
+  variable.name='Subtype',
+  value.name='Score',
+  # Get rid of sds!!
+  measure.vars=measures
+)
+# Get ymin, ymax for all of these variables
+binned.wide.multi$ymin <- rep(NA, nrow(binned.wide.multi))
+binned.wide.multi$ymax <- rep(NA, nrow(binned.wide.multi))
+for (i in grep('nms.*sd.*', names(binned.copy))) {
+  # Get rid of _sd_ to get the real name
+  name <- names(binned.copy)[[i]]
+  vals <- binned.copy[[name]]
+  orig <- gsub('_sd', '', name)
+  ymins <- binned.wide.multi[binned.wide.multi$Subtype == orig, ]$Score - vals
+  ymins[ymins < 0] <- 0
+  ymaxs <- binned.wide.multi[binned.wide.multi$Subtype == orig, ]$Score + vals
+  binned.wide.multi[binned.wide.multi$Subtype == orig, ]$ymin <- ymins
+  binned.wide.multi[binned.wide.multi$Subtype == orig, ]$ymax <- ymaxs
+}
+# Assume 9 categories here.
+binned.wide.multi$names <- rep(1:length(unique(binned.wide.multi$names)), nrow(binned.wide.multi) / 9)
+rm(binned.copy)
+
+# Create multi.nms, multi.nms.with.counts funcs ====
+multi.nms <- function(nms_str, save=FALSE, means=FALSE) {
+  colvec <- as.character(lapply(1:4, function(n) {paste(nms_str, '_k', n, sep='')}))
+  bwm.subset <- binned.wide.multi[binned.wide.multi$Subtype %in% colvec, ]
+  # Rename to subtypes - assumes 4 groups
+  bwm.subset$Subtype <- rep(
+    as.character(lapply(1:4, as.character)),
+  each=9)
+  pd <- position_dodge(width=0.2)
+  p <- ggplot(bwm.subset,
+    aes(x = names, y=Score, group=Subtype, color=Subtype, fill=Subtype)
+  ) +
+    geom_point(position=pd) +
+    geom_smooth(se=FALSE) +
+    geom_errorbar(aes(ymin=ymin, ymax=ymax), position=pd) +
+    scale_x_continuous(breaks=1:9, labels=rownames(binned))
+  # TODO: Melt to different format like above for p.bar; then position_dodge
+  # http://stats.stackexchange.com/questions/103393/how-can-i-dodge-the-position-of-geom-point-in-ggplot2
   
   # Offset. Might need to tweak it
   mean.offset <- .1
-  if (means) {
-    abline.df <- data.frame(intercept=subclus.means, slope=rep(0, 4), colour=as.factor(1:4), text.x=rep(nrow(binned) - 0.5, 4))
-    p <- p +
-      geom_abline(aes(intercept = intercept, slope=slope, colour=colour), data=abline.df, linetype='dashed') +
-      annotate("text", x=abline.df$text.x, y=abline.df$intercept + mean.offset,
-               label=paste("µ = ", round(abline.df$intercept, 2), sep=""), size=6, colour=gg_color_hue(4))
-  }
-  
+#   if (means) {
+#     abline.df <- data.frame(intercept=subclus.means, slope=rep(0, 4), colour=as.factor(1:4), text.x=rep(nrow(binned) - 0.5, 4))
+#     p <- p +
+#       geom_abline(aes(intercept = intercept, slope=slope, colour=colour), data=abline.df, linetype='dashed') +
+#       annotate("text", x=abline.df$text.x, y=abline.df$intercept + mean.offset,
+#                label=paste("µ = ", round(abline.df$intercept, 2), sep=""), size=6, colour=gg_color_hue(4))
+#   }
+#   
   # Global mean
   mean.nms <- mean(raw.omitted[[nms_str]])
   p <- p +
@@ -156,22 +186,22 @@ plot.nms <- function(nms_str, save=FALSE, means=FALSE) {
     theme_pub() +
     blank.theme +
     ggtitle(nms_str) +
+    ylab('Score') +
     theme(plot.margin = unit(c(1,1,0,1), "cm")) +
-    scale_x_discrete(breaks=1:nrow(binned), labels = rownames(binned)) +
     labs(colour="Subtype")
   
   if (save) {
-    dev.copy(pdf, paste('../figures/longitudinal/', nms_str, '-ALL-durat', '.pdf', sep=''),
+    dev.copy(pdf, paste('../figures/longitudinal/', nms_str, '-multi-durat', '.pdf', sep=''),
              width=14, height=10)
     dev.off()
   }
   p
 }
 
-plot.nms.with.counts <- function(nms_str, save=FALSE, means=FALSE) {
-  nms.plot <- plot.nms(nms_str, means=means)
-  gA=ggplot_gtable(ggplot_build(nms.plot))
-  gB=ggplot_gtable(ggplot_build(p.bar))
+multi.nms.with.counts <- function(nms_str, save=FALSE, means=FALSE) {
+  nms.plot <- multi.nms(nms_str, means=means)
+  gA <- ggplot_gtable(ggplot_build(nms.plot))
+  gB <- ggplot_gtable(ggplot_build(p.bar))
   maxWidth = grid::unit.pmax(gA$widths[2:5], gB$widths[2:5])
   gA$widths[2:5] <- as.list(maxWidth)
   gB$widths[2:5] <- as.list(maxWidth)
@@ -180,7 +210,7 @@ plot.nms.with.counts <- function(nms_str, save=FALSE, means=FALSE) {
     arrangeGrob(gA, gB, ncol=1, heights=c(.8,.3))
   )
   if (save) {
-    dev.copy(pdf, paste('../figures/longitudinal/', nms_str, '-ALL-durat-counts', '.pdf', sep=''),
+    dev.copy(pdf, paste('../figures/longitudinal/', nms_str, '-multi-durat-counts', '.pdf', sep=''),
              width=14, height=10)
     dev.off()
   }
@@ -356,6 +386,38 @@ ggplot(correlations.df, aes(x=names, y=r, fill=variable)) +
 #   ggsave('../figures/longitudinal/pd-durat-cor.pdf', width=14, height=10)
 # }
 
+# Everything: without binning ====
+durat.cor.everything <- function(symptom) cor(everything.wide$durat_pd, everything.wide[[symptom]])
+correlations.everything <- sapply(c(ALL.SYMPTOMS, NMS.30), durat.cor.everything)
+correlations.everything <- sort(correlations.everything)  # Sort ascending
+# Pretty meaningless - no negative correlations!!
+is_d.e <- grepl("d", names(correlations.everything))
+is_d.e[which(is_d.e == TRUE)] <- "nms_d{1-9}"
+is_d.e[which(is_d.e == FALSE)] <- "nms{1-30}"
+
+correlations.df.e <- data.frame(
+  names=names(correlations.everything),
+  r=correlations.everything,
+  variable=is_d.e
+)
+
+correlations.df.e$names <- factor(correlations.df.e$names,
+                                  levels=names(sort(correlations.everything)))
+ggplot(correlations.df.e, aes(x=names, y=r, fill=variable)) +
+  geom_bar(stat="identity", position="identity") +
+  geom_text(aes(label=round(r, 2)), position=position_dodge(width=0.9), vjust=2 * (correlations.df.e$r < 0) - .5) +
+  scale_y_continuous(limits = c(-1, 1)) +
+  ylab("r") +
+  xlab("Variable") +
+  guides(guides(fill=guide_legend(title="Variable Type"))) +
+  theme_pub() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  ggtitle("Correlation with PD duration")
+# Only save with different names!
+# if (SAVE.LONG.PLOTS) {
+#   ggsave('../figures/longitudinal/pd-durat-cor.pdf', width=14, height=10)
+# }
+
 # SEGMENTED regression ====
 library(segmented)
 
@@ -397,3 +459,6 @@ if (SAVE.LONG.PLOTS) {
   dev.copy(pdf, '../figures/longitudinal/nms10-resids.pdf', width=14, height=10)
   dev.off()
 }
+
+# Stop binning. Just plot normal stuff ====
+ggplot(everything.wide, aes(x=durat_pd))
